b6870cc1f15a5c8b8176eb93f6ccdb3bd92810b3
clabooo11 <claboo11.jm@gmail.com>
2016-11-27 19:59:00 +0800

    Add files via upload
    kindly check our modification

---
+++ Pizza/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--frame for log in-->
+<html>
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<link rel="stylesheet" href="new1.css">
+
+<body background= "images/bg.jpeg" align ="center" >
+<br><br><br><br><br><br><br><br><br>
+
+<iframe src="login.php" name = "frame1" height="600" width="39%" frameborder="50" align = "center"</iframe>
+</body>
+</html>
+
+
